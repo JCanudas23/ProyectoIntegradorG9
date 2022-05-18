c9aeff8b1c08d88615e80d72f16fac4ca7f18939
Juan D. Calle <laurac.zapata@iMac-de-Admin-2.local>
2022-05-18 01:18:39 -0500

    Subiendo Proyecto

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static("public"));
+
+app.listen(3030, ()=> {
+    console.log("Servidor Corriendo en http://localhost:3030");
+}); 
+
+app.get('/', function (req, res) {
+    let indexHtml = path.resolve(__dirname, "./views/index.html");
+    res.sendFile(indexHtml);
+});
+
+app.get('/register', function (req, res) {
+    let indexHtml = path.resolve(__dirname, "./views/register.html");
+    res.sendFile(indexHtml);
+});
+
+app.get('/login', function (req, res) {
+    let indexHtml = path.resolve(__dirname, "./views/login.html");
+    res.sendFile(indexHtml);
+});
+
+app.get('/login', function (req, res) {
+    let indexHtml = path.resolve(__dirname, "./views/productCart.html");
+    res.sendFile(indexHtml);
+});
+
+app.get('/login', function (req, res) {
+    let indexHtml = path.resolve(__dirname, "./views/productDetail.html");
+    res.sendFile(indexHtml);
+});
