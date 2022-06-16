a4538f98e18f1afeb66deeb6549cb0a6805c5cef
JCanudas23 <joaquin.canudas@gmail.com>
2022-06-16 15:31:40 -0300

    method-override app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require("method-override");
+
 app.use(express.static("public"));
+
+app.use(methodOverride("_method"));
 
 app.set ("view engine", "ejs");
 
 
 app.listen(3030, ()=> {
-    console.log("Aca comienza Lacez http://localhost:3030");
+    console.log("Servidor Corriendo en http://localhost:3030");
 }); 
 
 app.set("views", path.resolve(__dirname,"views"));
@@ -18,15 +22,13 @@ const productRouter = require ('./routers/productRouter');
 
 const userRouter = require ('./routers/userRouter');
 
-/* const adminRouter = require ('./routers/adminRouter') */
+
 
 app.use('/', indexRouter);
 
-app.use('/products', productRouter);
+app.use('/', productRouter);
 
-app.use('/user', userRouter);
-
-/* app.use('/', adminRouter); */
+app.use('/', userRouter);
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
