9260818f6813e69d98481790f0f7c1dce4db8c8a
juancalle94 <judacar2@gmail.com>
2022-09-03 00:19:08 -0500

    funcionalidad pop up newsletter

--- public/js/newsletter.js
+++ public/js/newsletter.js
@@ -4,15 +4,20 @@ const erroremail = document.querySelector("#erroremail");
 const popup = document.querySelector("#popup");
 
 formNewsletter.addEventListener("submit", (event) => {
+  event.preventDefault();
   let errors = [];
   if (newsletterinput.value == "") {
     erroremail.innerHTML = "";
     erroremail.innerHTML += "Debes ingresar un correo electronico";
     errors.push(1);
-  }
-  if (errors.length > 0) {
-    event.preventDefault();
   } else {
-    event.submit;
+    popup.classList.toggle("show");
+    body.classList.toggle("fijar-body");
+    setTimeout(function () {
+      popup.classList.toggle("show");
+      body.classList.toggle("fijar-body");
+    }, 3000);
+    erroremail.innerHTML = "";
+    newsletterinput.value = "";
   }
 });
